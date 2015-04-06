0679254fe73a2cbdb350a5fbc49121040e022fc1
Daniel Flax <dflax@yahoo.com>
2015-04-06 16:37:12 -0400

    Added player and gravity.

--- UberJump/GameScene.swift
+++ UberJump/GameScene.swift
@@ -9,37 +9,99 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+
+	// Layered Nodes
+	let backgroundNode = SKNode()
+	let midgroundNode = SKNode()
+	let foregroundNode = SKNode()
+	let hudNode = SKNode()
+
+	// Player
+	let player = SKNode()
+
+	// To Accommodate iPhone 6
+	let scaleFactor: CGFloat = 0.0
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+
+	override init(size: CGSize) {
+		super.init(size: size)
+		backgroundColor = SKColor.whiteColor()
+
+		// Add some gravity
+		physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+
+		scaleFactor = self.size.width / 320.0
+
+		// Create the game nodes
+		// Background
+		backgroundNode = createBackgroundNode()
+		addChild(backgroundNode)
+
+		// Foreground
+		foregroundNode = SKNode()
+		addChild(foregroundNode)
+
+		// Add the player
+		player = createPlayer()
+		foregroundNode.addChild(player)
+
+	}
+
+	func createBackgroundNode() -> SKNode {
+		// 1
+		// Create the node
+		let backgroundNode = SKNode()
+		let ySpacing = 64.0 * scaleFactor
+
+		// 2
+		// Go through images until the entire background is built
+		for index in 0...19 {
+
+			// 3
+			let node = SKSpriteNode(imageNamed:String(format: "Background%02d", index + 1))
+
+			// 4
+			node.setScale(scaleFactor)
+			node.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+			node.position = CGPoint(x: self.size.width / 2, y: ySpacing * CGFloat(index))
+
+			//5
+			backgroundNode.addChild(node)
+		}
+
+		// 6
+		// Return the completed background node
+		return backgroundNode
+	}
+
+	// Add the UberJumper
+	func createPlayer() -> SKNode {
+		let playerNode = SKNode()
+		playerNode.position = CGPoint(x: self.size.width / 2, y: 80.0)
+
+		let sprite = SKSpriteNode(imageNamed: "Player")
+		playerNode.addChild(sprite)
+
+		// 1
+		playerNode.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+
+		// 2
+		playerNode.physicsBody?.dynamic = true
+
+		// 3
+		playerNode.physicsBody?.allowsRotation = false
+
+		// 4
+		playerNode.physicsBody?.restitution = 1.0
+		playerNode.physicsBody?.friction = 0.0
+		playerNode.physicsBody?.angularDamping = 0.0
+		playerNode.physicsBody?.linearDamping = 0.0
+
+		return playerNode
+	}
+
 }
+
